2258b2ecda6ad1bb93722c8785b5e99bd5d4abc7
Splash <shchukin.vania@gmail.com>
2023-07-17 13:18:50 +0300

    created a directory of components and a file to export them

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+// Import components
+import './components/index'
+// Import main js file
 import './ts/app'
+// Import main scss file
 import './index.scss'
+// Import index.html
 import './index.html'
