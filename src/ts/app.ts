a1e21094ea8db8b4a1441d814bc4f50df79629cf
Splash <shchukin.vania@gmail.com>
2023-07-30 19:18:18 +0300

    made opening and closing the side menu of projects

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,1 +1,14 @@
+// Opening the side menu
 
+const sideMenuButton = document.querySelector('.header__button-menu') as HTMLButtonElement
+sideMenuButton.addEventListener('click', () => {
+    const projectSideMenu = document.querySelector('project-side-menu') as HTMLElement
+    const valueAttribute = projectSideMenu.getAttribute('open')
+    if (valueAttribute === 'true') {
+        projectSideMenu.setAttribute('open', 'false')
+    } else if (valueAttribute === 'false') {
+        projectSideMenu.setAttribute('open', 'true')
+    } else if (valueAttribute === null) {
+        projectSideMenu.setAttribute('open', 'false')
+    }
+})
