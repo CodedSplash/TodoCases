a85ebe3da4743db2cb6ce8c7f46bd10d1b4a75d9
Splash <shchukin.vania@gmail.com>
2023-07-30 15:24:35 +0300

    created an interfaces file

---
+++ src/ts/interfaces.ts
@@ -0,0 +1,29 @@
+export interface Element {
+  shadow: ShadowRoot
+  render(): void
+}
+
+export interface ProjectMenuInterface extends Element {
+  projectRenderer(): void
+  projectTemplate(project: ProjectInterface): string
+}
+
+export interface TasksInterface {
+  title: string,
+  description: string,
+  id: number,
+  accomplished: boolean,
+  priority: 'Приоритет 1' | 'Приоритет 2' | 'Приоритет 3' | 'Приоритет 4'
+}
+
+export interface ProjectInterface {
+  id: number
+  title: string
+  color: string
+  tasks: TasksInterface[]
+}
+
+export interface Popup extends Element {
+  closePopup(): void
+  addProject():void
+}
