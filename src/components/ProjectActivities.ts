c18a0430f78d20521bfc7f78ae5708a9c31703ee
Splash <shchukin.vania@gmail.com>
2023-08-01 14:17:37 +0300

    implemented the openCloseMenu method

--- src/components/ProjectActivities.ts
+++ src/components/ProjectActivities.ts
@@ -10,6 +10,10 @@ class ProjectActivities extends HTMLElement implements ContextMenu {
     public render(): void {
         const style = `
             <style>
+                .container {
+                    position: relative;
+                }
+            
                 .context-menu-button {
                     border: none;
                     background-color: transparent;
@@ -23,19 +27,70 @@ class ProjectActivities extends HTMLElement implements ContextMenu {
                 .context-menu-button:hover {
                     background-color: rgba(255,255,255,0.4);
                 }
+                
+                .context-menu {
+                    display: none;
+                    position: absolute;
+                    left: -100%;
+                    background-color: #fff;
+                    -webkit-box-shadow: 0 0 10px 0px rgba(34, 60, 80, 0.2);
+                    -moz-box-shadow: 0 0 10px 0px rgba(34, 60, 80, 0.2);
+                    box-shadow: 0 0 10px 0px rgba(34, 60, 80, 0.2);
+                    padding: 6px 8px;
+                    border-radius: 5px;
+                    border: 1px solid rgba(255, 255, 255, 0.1);
+                }
+                
+                .context-menu.open {
+                    display: block;
+                }
+                
+                .context-menu__item {
+                    width: 100%;
+                    text-align: left;
+                    padding: 9px 5px;
+                    background-color: transparent;
+                    border: none;
+                    border-radius: 4px;
+                    cursor: pointer;
+                    font-weight: 700;
+                }
+                
+                .context-menu__item:hover {
+                    background-color: rgba(47,79,79,0.25);
+                }
             </style>
         `
 
         this.shadow.innerHTML = `
             ${style}
-            <button class="context-menu-button">
-                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 128 512"><style>svg{fill:#000000}</style><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>
-            </button>
+            <div class="container">
+                <button class="context-menu-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 128 512"><style>svg{fill:#000000}</style><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>
+                </button>
+                <div class="context-menu">
+                    <div class="context-menu__body">
+                        <button class="context-menu__item">Изменить</button>
+                        <button class="context-menu__item">Дублировать</button>
+                        <button class="context-menu__item">Удалить</button>
+                    </div>
+                </div>
+            </div>
         `
+    }
+
+    public openCloseMenu(): void {
+        const contextMenu = this.shadow.querySelector('.context-menu') as HTMLDivElement
+        contextMenu.classList.toggle('open')
     }
 
     public connectedCallback():void {
         this.render()
+        this.shadow.querySelector('button.context-menu-button')!.addEventListener('click', this.openCloseMenu.bind(this))
+    }
+
+    public disconnectedCallback(): void {
+        this.shadow.querySelector('.context-menu-button')!.removeEventListener('click', this.openCloseMenu)
     }
 }
 
