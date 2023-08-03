734de9871cf3a678376343640a3d0f1f945bbfa2
Splash <shchukin.vania@gmail.com>
2023-08-03 11:17:08 +0300

    stylized the component

--- src/components/AddTaskForm.ts
+++ src/components/AddTaskForm.ts
@@ -10,7 +10,81 @@ class AddTaskForm extends HTMLElement implements AddTaskFormInterface {
     public render(): void {
         const style = `
             <style>
+                .add-task-form {
+                    border: 1px solid #c0c0c0;
+                    border-radius: 10px;
+                }
                 
+                .add-task-form__content {
+                    display: flex;
+                    flex-direction: column;
+                    row-gap: 8px;
+                    padding: 8px 8px 0 8px;
+                }
+                
+                .add-task-form__content select {
+                    align-self: start;
+                }
+                
+                .add-task-form__content input {
+                    border: none;
+                }
+                
+                .add-task-form__content input:focus {
+                    outline: none;
+                }
+                
+                #name {
+                    font-size: 14px;
+                    font-weight: 700;
+                }
+                
+                #description {
+                    font-size: 12px;
+                }
+                
+                .add-task-form__buttons {
+                    display: flex;
+                    align-items: center;
+                    justify-content: end;
+                    column-gap: 10px;
+                    padding: 0 8px 8px 8px;
+                }
+                
+                .add-task-form__cancel {
+                    background-color: #2F4F4FFF;
+                    border-radius: 4px;
+                    border: none;
+                    padding: 10px;
+                    color: #fff;
+                    font-weight: 700;
+                    cursor: pointer;
+                    -webkit-box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                    -moz-box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                    box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                }
+                
+                .add-task-form__add {
+                    background-color: #ff004d;
+                    border-radius: 4px;
+                    border: none;
+                    padding: 10px;
+                    color: #fff;
+                    font-weight: 700;
+                    cursor: pointer;
+                    -webkit-box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                    -moz-box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                    box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                }
+                
+                .add-task-form__add:disabled {
+                    background-color: rgba(255,0,77,0.7);
+                    cursor: default;
+                  }
+                
+                hr {
+                    border: 1px solid #c0c0c0;
+                }
             </style>
         `
         this.shadow.innerHTML = `
