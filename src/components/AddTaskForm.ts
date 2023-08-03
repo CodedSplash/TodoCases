367c0e62b38d2ad286487d19048864b3e1a42132
Splash <shchukin.vania@gmail.com>
2023-08-03 10:59:25 +0300

    implemented the render method

--- src/components/AddTaskForm.ts
+++ src/components/AddTaskForm.ts
@@ -7,10 +7,38 @@ class AddTaskForm extends HTMLElement implements AddTaskFormInterface {
         this.shadow = this.attachShadow({mode: 'open'})
     }
 
-    
+    public render(): void {
+        const style = `
+            <style>
+                
+            </style>
+        `
+        this.shadow.innerHTML = `
+            ${style}
+            <div class="add-task-form">
+                <div class="add-task-form__conteiner">
+                    <div class="add-task-form__content">
+                        <input type="text" id="name" placeholder="Название задачи">
+                        <input type="text" id="description" placeholder="Описание">
+                        <select name="priority" id="priority">
+                            <option value="Приоритет 1">Приоритет 1</option>
+                            <option value="Приоритет 2">Приоритет 2</option>
+                            <option value="Приоритет 3">Приоритет 3</option>
+                            <option value="Приоритет 4" selected>Приоритет 4</option>
+                        </select>
+                    </div>
+                    <hr>
+                    <div class="add-task-form__buttons">
+                        <button class="add-task-form__cancel">Отмена</button>
+                        <button class="add-task-form__add">Добавить задачу</button>
+                    </div>
+                </div>
+            </div>
+        `
+    }
 
     public connectedCallback(): void {
-
+        this.render()
     }
 }
 
