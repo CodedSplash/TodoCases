1f44c3a556e83a50c85f64402c0653699d89f7b5
Splash <shchukin.vania@gmail.com>
2023-08-06 16:46:14 +0300

    implemented the render method

--- src/components/ActionsTask.ts
+++ src/components/ActionsTask.ts
@@ -7,8 +7,85 @@ class ActionsTask extends HTMLElement implements ActionsTaskInterface {
         this.shadow = this.attachShadow({mode: 'open'})
     }
 
-    public connectedCallback(): void {
+    public render(): void {
+        const style = `
+           <style>
+                .container {
+                    position: relative;
+                }
+            
+                .context-menu-button {
+                    border: none;
+                    background-color: transparent;
+                    padding: 10px 15px;
+                    cursor: pointer;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-radius: 6px;
+                }
+                
+                .context-menu-button:hover {
+                    background-color: rgba(0,0,0,0.15);
+                }
+                
+                .context-menu {
+                    display: none;
+                    position: absolute;
+                    left: -100%;
+                    background-color: #fff;
+                    -webkit-box-shadow: 0 0 10px 0px rgba(34, 60, 80, 0.2);
+                    -moz-box-shadow: 0 0 10px 0px rgba(34, 60, 80, 0.2);
+                    box-shadow: 0 0 10px 0px rgba(34, 60, 80, 0.2);
+                    padding: 6px 8px;
+                    border-radius: 5px;
+                    border: 1px solid rgba(255, 255, 255, 0.1);
+                    z-index: 10;
+                }
+                
+                .context-menu.open {
+                    display: block;
+                }
+                
+                .context-menu__item {
+                    width: 100%;
+                    text-align: left;
+                    padding: 9px 5px;
+                    background-color: transparent;
+                    border: none;
+                    border-radius: 4px;
+                    cursor: pointer;
+                    font-weight: 700;
+                }
+                
+                .context-menu__item:hover {
+                    background-color: rgba(47,79,79,0.25);
+                }
+            </style>
+        `
 
+        this.shadow.innerHTML = `
+            ${style}
+            <div class="container">
+                <button class="context-menu-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 128 512"><style>svg{fill:#000000}</style><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>
+                </button>
+                <div class="context-menu">
+                    <div class="context-menu__body">
+                        <button class="context-menu__item" id="modify">Изменить</button>
+                        <changing-priority></changing-priority>
+                        <button class="context-menu__item" id="duplicate">Дублировать</button>
+                        <button class="context-menu__item" id="delete">Удалить</button>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+
+    
+
+    public connectedCallback(): void {
+        this.render()
     }
 }
 
