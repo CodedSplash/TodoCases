37664cc944dd8e1e431fe9d83aedc88ca2d1331d
Splash <shchukin.vania@gmail.com>
2023-08-06 18:01:33 +0300

    implemented the render method

--- src/components/ModifyTask.ts
+++ src/components/ModifyTask.ts
@@ -7,8 +7,219 @@ class ModifyTask extends HTMLElement implements ModifyTaskInterface {
         this.shadow = this.attachShadow({mode: 'open'})
     }
 
-    public connectedCallback(): void {
+    public render(): void {
+        const style = `
+            <style>
+                .popup {
+                    background-color: rgba(0, 0, 0, .5);
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    z-index: 19;
+                    width: 100%;
+                    height: 100vh;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    overflow: hidden;
+                  }
+                  .popup__body {
+                    background-color: #fff;
+                    border-radius: 10px;
+                    display: flex;
+                    flex-direction: column;
+                    row-gap: 14px;
+                    min-width: 500px;
+                    max-height: 600px;
+                    max-width: 600px;
+                    overflow-y: auto;
+                    overflow-x: hidden;
+                    -webkit-box-shadow: 0 5px 10px 2px rgba(34, 60, 80, 0.2);
+                    -moz-box-shadow: 0 5px 10px 2px rgba(34, 60, 80, 0.2);
+                    box-shadow: 0 5px 10px 2px rgba(34, 60, 80, 0.2);
+                    padding: 15px 0;
+                  }
+                  
+                  .popup__content {
+                    display: flex;
+                    flex-direction: column;
+                    row-gap: 20px;
+                    padding: 0 20px;
+                  }
+                  
+                  .popup__input {
+                    display: flex;
+                    flex-direction: column;
+                    row-gap: 10px;
+                  }
+                  
+                  .popup__input label {
+                    font-weight: 700;
+                  }
+                  
+                  .popup__input input {
+                    font-size: 18px;
+                    border-radius: 4px;
+                    outline: none;
+                  }
+                  
+                  .popup__input input:focus {
+                    border: 2px solid rgba(0, 0, 0, 0.5);
+                    outline: none;
+                  }
+                  
+                  #name {
+                    padding: 5px;
+                  }
+                  
+                  #description {
+                    padding: 5px;
+                  }
+                  
+                  .popup__textarea {
+                    display: flex;
+                    flex-direction: column;
+                    row-gap: 10px;
+                  }
+                  
+                  .popup__textarea textarea {
+                    font-size: 18px;
+                    border-radius: 4px;
+                    outline: none;
+                    resize: none;
+                  }
+                  
+                  .popup__input textarea:focus {
+                    border: 2px solid rgba(0, 0, 0, 0.5);
+                    outline: none;
+                  }
+                  
+                  .popup__textarea label {
+                    font-weight: 700;
+                  }
+                  
+                  .popup__priority {
+                    display: flex;
+                    flex-direction: column;
+                    row-gap: 10px;
+                  }
+                  
+                  .popup__priority select {
+                    align-self: start;
+                  }
+                  
+                  .popup__priority label {
+                    font-weight: 700;
+                  }
+                  
+                  .popup__buttons {
+                    display: flex;
+                    justify-content: end;
+                    align-items: center;
+                    column-gap: 10px;
+                    padding: 0 20px;
+                  }
+                  
+                  hr {
+                    width: 100%;
+                    padding: 0;
+                    margin: 0;
+                    border-color: rgba(211, 211, 211, 0.4);
+                  }
+                  
+                  .popup__title {
+                    margin: 0;
+                    padding: 0 20px;
+                  }
+                  
+                  .popup__cancel {
+                    background-color: #2F4F4FFF;
+                    border-radius: 4px;
+                    border: none;
+                    padding: 10px;
+                    color: #fff;
+                    font-weight: 700;
+                    cursor: pointer;
+                    -webkit-box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                    -moz-box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                    box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                  }
+                  
+                  .popup__save-btn {
+                    background-color: #ff004d;
+                    border-radius: 4px;
+                    border: none;
+                    padding: 10px;
+                    color: #fff;
+                    font-weight: 700;
+                    cursor: pointer;
+                    -webkit-box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                    -moz-box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                    box-shadow: 0 0 4px 0 rgba(34, 60, 80, 0.2);
+                  }
+                  .popup__save-btn:disabled {
+                    background-color: rgba(255,0,77,0.7);
+                    cursor: default;
+                  }
+                  
+                  @media (max-width: 520px) {
+                    .popup__body {
+                        min-width: 290px;
+                        max-width: 290px;
+                    }
+                    
+                    .popup__input input {
+                      font-size: 12px;
+                    }
+                  }
+            </style>
+        `
 
+        this.shadow.innerHTML = `
+            ${style}
+            <div class="popup">
+              <div class="popup__body">
+                <h2 class="popup__title">Изменить</h2>
+                <hr>
+                <div class="popup__content">
+                  <div class="popup__input">
+                      <label for="name">Название задачи</label>
+                      <input type="text" name="name" id="name" />
+                  </div>
+                  <div class="popup__textarea">
+                      <label for="description">Описание задачи</label>
+                      <textarea name="description" id="description" rows="6"></textarea>
+                  </div>
+                  <div class="popup__priority">
+                      <label for="priority">Приоритет задачи</label>
+                      <select name="priority" id="priority">
+                        <option value="Приоритет 1">Приоритет 1</option>
+                        <option value="Приоритет 2">Приоритет 2</option>
+                        <option value="Приоритет 3">Приоритет 3</option>
+                        <option value="Приоритет 4" selected>Приоритет 4</option>
+                      </select>
+                  </div>
+                </div>
+                <hr>
+                <div class="popup__buttons">
+                    <button class="popup__cancel">Отмена</button>
+                    <button class="popup__save-btn" disabled>Сохранить</button>
+                </div>
+              </div>
+            </div>
+        `
+    }
+
+    public closePopup(): void {
+
+    }
+
+    public modifyTask(): void {
+
+    }
+
+    public connectedCallback(): void {
+        this.render()
     }
 }
 
