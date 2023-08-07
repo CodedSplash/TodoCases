cef9c6340b7babfc9cc2652350a82925fedc8973
Splash <shchukin.vania@gmail.com>
2023-08-07 19:21:52 +0300

    implemented the render method

--- src/components/ThemeItem.ts
+++ src/components/ThemeItem.ts
@@ -7,8 +7,87 @@ class ThemeItem extends HTMLElement implements ThemeItemInterface {
         this.shadow = this.attachShadow({mode: 'open'})
     }
 
-    public connectedCallback(): void {
+    public render(): void {
+        let color: string | undefined
+        let textButton: string | undefined
+        const theme: string = this.getAttribute('theme-name')!
+        const checked: string = this.getAttribute('checked')!
+        const isChecked: boolean = checked !== 'false'
 
+        if (theme === 'dark') {
+            color = '#444444'
+            textButton = 'Чёрная тема'
+        } else if (theme === 'blue') {
+            color = '#003AFF'
+            textButton = 'Синяя тема'
+        } else if (theme === 'red') {
+            color = '#FF0046'
+            textButton = 'Красная тема'
+        } else if (theme === 'white') {
+            color = '#fff'
+            textButton = 'Белая тема'
+        }
+
+
+        const style = `
+            <style>
+                button {
+                    display: flex;
+                    align-items: center;
+                    column-gap: 8px;
+                    width: 100%;
+                    background-color: ${color};
+                    border-radius: 10px;
+                    padding: 20px;
+                    font-weight: 700;
+                    border: 1px solid #DEDEDE;
+                    cursor: pointer;
+                    color: ${color === '#fff' ? '#000' : '#fff'};
+                    -webkit-box-shadow: 2px 2px 15px -8px rgba(34, 60, 80, 0.2);
+                    -moz-box-shadow: 2px 2px 15px -8px rgba(34, 60, 80, 0.2);
+                    box-shadow: 2px 2px 15px -8px rgba(34, 60, 80, 0.2);
+                }
+                
+                button span {
+                    display: block;
+                }
+                
+                button input[type=radio] {
+                    margin: 0;
+                }
+            </style>
+        `
+
+        this.shadow.innerHTML = `
+            ${style}
+            <button>
+                <input type="radio" ${isChecked ? 'checked' : ''}>
+                <span>${textButton}</span>
+            </button>
+        `
+    }
+
+    public closePopup(): void {
+    }
+
+    public white(): void {
+
+    }
+
+    public black(): void {
+
+    }
+
+    public blue(): void {
+
+    }
+
+    public red(): void {
+
+    }
+
+    public connectedCallback(): void {
+        this.render()
     }
 }
 
